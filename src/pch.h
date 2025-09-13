b5573b4adb0504730cfd517fbefcf9fdc86b9320
Denis Klintukhov <denis.klintukhov@gmail.com>
2025-09-13 15:19:03 +0300

    Add: CMake project

---
+++ src/pch.h
@@ -0,0 +1,28 @@
+/*
+ *  This file is part of nzbget. See <https://github.com/DKlintukhov/cmake-init>.
+ *
+ *  Copyright (C) 2025 Denis <denis.klintukhov@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <format>
+#include <print>
+#include <filesystem>
+#include <chrono>
+#include <unordered_map>
